b1547596905175b89eb5072f9b763baf8961ea8d
wesleylo <weslo404@gmail.com>
2016-02-22 22:58:18 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+});
+
+$(window).load(function(){
+	$('.slider').fractionSlider({
+		'fullWidth': 			true,
+		'controls': 			true, 
+		'pager': 				true,
+		'responsive': 			true,
+		'dimensions': 			"1651,708",
+	    'increase': 			false,
+		'pauseOnHover': 		true,
+		'backgroundAnimation':  true,
+		'backgroundY': 			'-700',
+		'backgroundX': 			'0',
+	});
+
+});
